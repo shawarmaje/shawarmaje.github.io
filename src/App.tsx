d2fd61c9a1836c5dfd71ee51eced176150e87aad
shawarmaje <shawarma.alhakim@gmail.com>
2022-09-13 21:58:25 +0300

    üpdated website

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ⚠ This Website Is Under Construction ⚠
-        </p>
-      </header>
+    <div className="container">
+      <p className="title">Hussein Amine</p>
+      <div className="title-divider"></div>
+      <div className="paragraph">
+        <a className="social-link" href="https://github.com/shawarmaje">Github</a>
+        <a className="social-link" href="https://discord.com/users/562895658361815040">Discord</a>
+        <a className="social-link" href="https://instagram.com/husseinamine">Instagram</a>
+      </div>
+
+      <div className="divider"></div>
+
+      <p className="title">About Me</p>
+      <div className="title-divider"></div>
+      <p className="paragraph">Hello. my name is Hussein, this is my website, still working on it, will finish soon!</p>
     </div>
   );
 }

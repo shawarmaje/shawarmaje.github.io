9e5e06854b1838217fcc1516761f9fccf2574f11
shawarmaje <shawarma.alhakim@gmail.com>
2022-09-14 15:17:49 +0300

    added Contact Me button

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <p className="title">Hussein Amine</p>
-      <div className="title-divider"></div>
+      <div style={{display: "flex", justifyContent: "space-between", alignItems: "flex-end"}}>
+        <p className="title">Hussein Amine</p>
+        <button 
+          className="btn"
+          onClick={(e) => {
+            window.location.href = 'mailto:contact@husseinamine.com';
+            e.preventDefault();
+          }}>Contact Me</button>
+      </div>
       <div className="paragraph">
         <a className="social-link" href="https://github.com/shawarmaje">Github</a>
         <a className="social-link" href="https://discord.com/users/562895658361815040">Discord</a>
         <a className="social-link" href="https://instagram.com/husseinamine">Instagram</a>
       </div>
 
-      <div className="divider"></div>
-
       <p className="title">About Me</p>
-      <div className="title-divider"></div>
       <p className="paragraph">Hello. my name is Hussein, this is my website, still working on it, will finish soon!</p>
     </div>
   );
